04c2882b29793004979ae69675921d58f9083d28
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-07-08 20:00:28 -0300

    Código del Cuadrado Listo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # practicoJS
 Practicas de JavaScript
+
+## Taller #1: figuras geométricas
+
+- Primer paso: definir fórmulas
+- Segundo paso: implementar las fórmulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JavaScript con HTML 
---
+++ figuras.js
@@ -0,0 +1,13 @@
+/* CÓDIGO DEL CUADRADO */
+
+const ladoCuadrado = 5;
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log(`El Perimétro del cuadrado es: ${perimetroCuadrado} cm.`);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log(`El Área del cuadrado es: ${areaCuadrado} cm^2.`);
+
+/* CÓDIGO DEL TRIANGULO */
+
+/* CÓDIGO DEL CIRCULO */
