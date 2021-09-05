a4a1ed662a25ac4a06cee5cc779a8fd3306f4ded
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-09-05 01:49:54 -0300

    Data OK

--- salarios.js
+++ salarios.js
@@ -1,101 +1,101 @@
 const argentina = [];
 
 argentina.push({
+  name: "Mayra",
+  salary: 60000
+});
+
+argentina.push({
+  name: "Alberto",
+  salary: 7000
+});
+
+argentina.push({
+  name: "Gustavo",
+  salary: 1100
+});
+
+argentina.push({
+  name: "Ivana",
+  salary: 9000
+});
+
+argentina.push({
+  name: "beatriz",
+  salary: 6000
+});
+
+argentina.push({
+  name: "Pablo",
+  salary: 1000
+});
+argentina.push({
   name: "Juanita",
+  salary: 5000
+});
+
+argentina.push({
+  name: "Carlos",
   salary: 1000
 });
 
 argentina.push({
-  name: "Juanita",
-  salary: 1000
+  name: "Mariano",
+  salary: 9000
 });
 
 argentina.push({
-  name: "Juanita",
-  salary: 1000
+  name: "Marcelo",
+  salary: 10000
 });
 
 argentina.push({
-  name: "Juanita",
-  salary: 1000
+  name: "Guillermo",
+  salary: 5000
 });
 
 argentina.push({
-  name: "Juanita",
-  salary: 1000
+  name: "Batman",
+  salary: 8000
 });
 
 argentina.push({
-  name: "Juanita",
-  salary: 1000
-});
-argentina.push({
-  name: "Juanita",
-  salary: 1000
-});
-
-argentina.push({
-  name: "Juanita",
-  salary: 1000
-});
-
-argentina.push({
-  name: "Juanita",
-  salary: 1000
-});
-
-argentina.push({
-  name: "Juanita",
-  salary: 1000
-});
-
-argentina.push({
-  name: "Juanita",
-  salary: 1000
-});
-
-argentina.push({
-  name: "Juanita",
-  salary: 1000
-});
-
-argentina.push({
-  name: "Juanita",
-  salary: 1000
+  name: "Hideo",
+  salary: 10000
 });
 
 
 argentina.push({
-  name: "Juanita",
+  name: "Elon",
+  salary: 7000
+});
+
+argentina.push({
+  name: "Ricardo",
   salary: 1000
 });
 
 argentina.push({
-  name: "Juanita",
+  name: "Dua",
   salary: 1000
 });
 
 argentina.push({
-  name: "Juanita",
-  salary: 1000
+  name: "Mr. X",
+  salary: 2000
 });
 
 argentina.push({
-  name: "Juanita",
-  salary: 1000
+  name: "Chris",
+  salary: 3000
 });
 
 argentina.push({
-  name: "Juanita",
-  salary: 1000
+  name: "Dave",
+  salary: 8500
 });
 
 argentina.push({
-  name: "Juanita",
-  salary: 1000
-});
-
-argentina.push({
-  name: "Juanita",
+  name: "Juanito",
   salary: 1000
 });
